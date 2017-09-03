9842fe2ae93082da2516cf0c44eea15eac34368e
songzhanlong <song_zhanlong@hotmail.com>
2017-09-03 15:00:37 +0800

    @Autowired 的（required=false）选项 默认情况下，@Autowired 注释意味着依赖是必须的，它类似于 @Required 注释，然而，你可以使用 @Autowired 的 （required=false） 选项关闭默认行为。

--- springdemo/src/main/java/com/szl/demo/spring/springdemo/Student.java
+++ springdemo/src/main/java/com/szl/demo/spring/springdemo/Student.java
@@ -1,12 +1,12 @@
 package com.szl.demo.spring.springdemo;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Student {
 	private Integer age;
 	private String name;
 
-	@Required
+	@Autowired(required = false)
 	public void setAge(Integer age) {
 		this.age = age;
 	}
@@ -15,7 +15,7 @@ public class Student {
 		return age;
 	}
 
-	@Required
+	@Autowired
 	public void setName(String name) {
 		this.name = name;
 	}
