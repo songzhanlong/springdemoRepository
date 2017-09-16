13968aa441de6864ca5a78f8353a9e5a2f8a81fa
songzhanlong <song_zhanlong@hotmail.com>
2017-09-16 08:44:42 +0800

    Spring JDBC 示例

--- springdemo/src/main/java/com/szl/demo/spring/springdemo/App.java
+++ springdemo/src/main/java/com/szl/demo/spring/springdemo/App.java
@@ -1,5 +1,7 @@
 package com.szl.demo.spring.springdemo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -29,8 +31,25 @@ public class App {
 
 	private static void runClassPathXmlApplicationContext(String configPath) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(configPath);
-		HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
-		System.out.println(helloWorld.getMessage());
+		StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean("studentJDBCTemplate");
+		System.out.println("------Records Creation--------");
+		studentJDBCTemplate.create("Zara", 11);
+		studentJDBCTemplate.create("Nuha", 2);
+		studentJDBCTemplate.create("Ayan", 15);
+		System.out.println("------Listing Multiple Records--------");
+		List<Student> students = studentJDBCTemplate.listStudents();
+		for (Student record : students) {
+			System.out.print("ID : " + record.getId());
+			System.out.print(", Name : " + record.getName());
+			System.out.println(", Age : " + record.getAge());
+		}
+		System.out.println("----Updating Record with ID = 2 -----");
+		studentJDBCTemplate.update(2, 20);
+		System.out.println("----Listing Record with ID = 2 -----");
+		Student student = studentJDBCTemplate.getStudent(2);
+		System.out.print("ID : " + student.getId());
+		System.out.print(", Name : " + student.getName());
+		System.out.println(", Age : " + student.getAge());
 	}
 
 	private static void runFileSystemXmlApplicationContext(String configPath) {
