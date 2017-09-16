f5704fd7dfd4a743ed11f07a83053ff082f9fb19
songzhanlong <song_zhanlong@hotmail.com>
2017-09-16 08:12:20 +0800

    Spring 中基于 AOP 的 @AspectJ

--- springdemo/src/main/java/com/szl/demo/spring/springdemo/Logging.java
+++ springdemo/src/main/java/com/szl/demo/spring/springdemo/Logging.java
@@ -1,10 +1,26 @@
 package com.szl.demo.spring.springdemo;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class Logging {
+	/**
+	 * Following is the definition for a pointcut to select all the methods
+	 * available. So advice will be called for all the methods.
+	 */
+	@Pointcut("execution(* com.szl.demo.spring.springdemo.*.*(..))")
+	private void selectAll() {
+	}
+
 	/**
 	 * This is the method which I would like to execute before a selected method
 	 * execution.
 	 */
+	@Before("selectAll()")
 	public void beforeAdvice() {
 		System.out.println("Going to setup student profile.");
 	}
@@ -13,6 +29,7 @@ public class Logging {
 	 * This is the method which I would like to execute after a selected method
 	 * execution.
 	 */
+	@After("selectAll()")
 	public void afterAdvice() {
 		System.out.println("Student profile has been setup.");
 	}
@@ -20,6 +37,7 @@ public class Logging {
 	/**
 	 * This is the method which I would like to execute when any method returns.
 	 */
+	@AfterReturning(pointcut = "selectAll()", returning = "retVal")
 	public void afterReturningAdvice(Object retVal) {
 		System.out.println("Returning:" + retVal.toString());
 	}
