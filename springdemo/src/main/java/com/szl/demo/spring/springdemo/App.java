d92706b77a7ebca5df057fe09c547dec2aabd685
songzhanlong <song_zhanlong@hotmail.com>
2017-09-18 00:36:17 +0800

    Spring 编程式事务管理
    编程式事务管理方法允许你在对你的源代码编程的帮助下管理事务。这给了你极大地灵活性，但是它很难维护。

--- springdemo/src/main/java/com/szl/demo/spring/springdemo/App.java
+++ springdemo/src/main/java/com/szl/demo/spring/springdemo/App.java
@@ -16,11 +16,11 @@ import org.springframework.core.io.ClassPathResource;
 public class App {
 	public static void main(String[] args) {
 		String configPath = "beans.xml";
-		runBeanFactory(configPath);
+		// runBeanFactory(configPath);
 		runClassPathXmlApplicationContext(configPath);
 		// mac上只能用相对该项目根目录的PATH，window系统中可以从磁盘根目录的路径如d:/...
-		configPath = "/src/main/resource/beans.xml";
-		runFileSystemXmlApplicationContext(configPath);
+		// configPath = "/src/main/resource/beans.xml";
+		// runFileSystemXmlApplicationContext(configPath);
 	}
 
 	private static void runBeanFactory(String configPath) {
@@ -32,24 +32,19 @@ public class App {
 	private static void runClassPathXmlApplicationContext(String configPath) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(configPath);
 		StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean("studentJDBCTemplate");
-		System.out.println("------Records Creation--------");
-		studentJDBCTemplate.create("Zara", 11);
-		studentJDBCTemplate.create("Nuha", 2);
-		studentJDBCTemplate.create("Ayan", 15);
-		System.out.println("------Listing Multiple Records--------");
-		List<Student> students = studentJDBCTemplate.listStudents();
-		for (Student record : students) {
+		System.out.println("------Records creation--------");
+		studentJDBCTemplate.create("Zara", 11, 99, 2010);
+		studentJDBCTemplate.create("Nuha", 20, 97, 2010);
+		studentJDBCTemplate.create("Ayan", 25, 100, 2011);
+		System.out.println("------Listing all the records--------");
+		List<StudentMarks> studentMarks = studentJDBCTemplate.listStudents();
+		for (StudentMarks record : studentMarks) {
 			System.out.print("ID : " + record.getId());
 			System.out.print(", Name : " + record.getName());
+			System.out.print(", Marks : " + record.getMarks());
+			System.out.print(", Year : " + record.getYear());
 			System.out.println(", Age : " + record.getAge());
 		}
-		System.out.println("----Updating Record with ID = 2 -----");
-		studentJDBCTemplate.update(2, 20);
-		System.out.println("----Listing Record with ID = 2 -----");
-		Student student = studentJDBCTemplate.getStudent(2);
-		System.out.print("ID : " + student.getId());
-		System.out.print(", Name : " + student.getName());
-		System.out.println(", Age : " + student.getAge());
 	}
 
 	private static void runFileSystemXmlApplicationContext(String configPath) {
--- springdemo/src/main/java/com/szl/demo/spring/springdemo/StudentDAO.java
+++ springdemo/src/main/java/com/szl/demo/spring/springdemo/StudentDAO.java
@@ -12,30 +12,14 @@ public interface StudentDAO {
 	public void setDataSource(DataSource ds);
 
 	/**
-	 * This is the method to be used to create a record in the Student table.
+	 * This is the method to be used to create a record in the Student and Marks
+	 * tables.
 	 */
-	public void create(String name, Integer age);
-
-	/**
-	 * This is the method to be used to list down a record from the Student
-	 * table corresponding to a passed student id.
-	 */
-	public Student getStudent(Integer id);
+	public void create(String name, Integer age, Integer marks, Integer year);
 
 	/**
 	 * This is the method to be used to list down all the records from the
-	 * Student table.
+	 * Student and Marks tables.
 	 */
-	public List<Student> listStudents();
-
-	/**
-	 * This is the method to be used to delete a record from the Student table
-	 * corresponding to a passed student id.
-	 */
-	public void delete(Integer id);
-
-	/**
-	 * This is the method to be used to update a record into the Student table.
-	 */
-	public void update(Integer id, Integer age);
+	public List<StudentMarks> listStudents();
 }
